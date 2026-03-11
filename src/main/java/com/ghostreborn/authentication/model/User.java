5f7fefe1f1810d3041341061d472cb3937719266
AbhinandAmmanamkandy <abhinandammanamkandy@gmail.com>
2026-03-11 16:32:04 +0530

    User checkup algorithm in CustomUserService * Lombok - Getters and Setters

--- src/main/java/com/ghostreborn/authentication/model/User.java
+++ src/main/java/com/ghostreborn/authentication/model/User.java
@@ -3,9 +3,11 @@ package com.ghostreborn.authentication.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
 @Table(name = "users")
+@Data
 public class User {
 
     @Id
--- src/main/java/com/ghostreborn/authentication/service/CustomUserService.java
+++ src/main/java/com/ghostreborn/authentication/service/CustomUserService.java
@@ -1,6 +1,8 @@
 package com.ghostreborn.authentication.service;
 
+import com.ghostreborn.authentication.model.User;
 import com.ghostreborn.authentication.repository.UserRepository;
+import org.jspecify.annotations.NonNull;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -16,7 +18,16 @@ public class CustomUserService implements UserDetailsService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(@NonNull String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+
+        if (user == null)
+            throw new UsernameNotFoundException("User not found");
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .roles("USER")
+                .build();
     }
 }
