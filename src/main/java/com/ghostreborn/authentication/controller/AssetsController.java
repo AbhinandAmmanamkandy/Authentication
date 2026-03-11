bef9eb35badf49a3c599ba49c9dafd52ba79a119
AbhinandAmmanamkandy <abhinandammanamkandy@gmail.com>
2026-03-11 15:44:07 +0530

    RequestMapping to get all Assets (Stub implementation)

--- src/main/java/com/ghostreborn/authentication/controller/AssetsController.java
+++ src/main/java/com/ghostreborn/authentication/controller/AssetsController.java
@@ -1,6 +1,7 @@
 package com.ghostreborn.authentication.controller;
 
 import com.ghostreborn.authentication.model.Asset;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +14,15 @@ public class AssetsController {
 
     ArrayList<Asset> testAssets = new ArrayList<>(
             List.of(
-                    new Asset(1L, "Asset 1")
+                    new Asset(1L, "Asset 1"),
+                    new Asset(2L, "Asset 2"),
+                    new Asset(3L, "Asset 3")
             )
     );
+
+    @GetMapping
+    public List<Asset> getAll() {
+        return testAssets;
+    }
 
 }
