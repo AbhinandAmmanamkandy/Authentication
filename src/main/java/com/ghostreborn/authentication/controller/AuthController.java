6fa5465619036a21eb4100e7319a011c25d8d64c
AbhinandAmmanamkandy <abhinandammanamkandy@gmail.com>
2026-03-11 15:57:12 +0530

    Create controller for authentication

---
+++ src/main/java/com/ghostreborn/authentication/controller/AuthController.java
@@ -0,0 +1,13 @@
+package com.ghostreborn.authentication.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+
+
+}
