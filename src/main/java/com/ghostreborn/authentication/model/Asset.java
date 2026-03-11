bfa0d92f2430e565009a9d780f35ba3a60d03b90
AbhinandAmmanamkandy <abhinandammanamkandy@gmail.com>
2026-03-11 15:32:09 +0530

    Mark Asset as Entity for persistence

--- src/main/java/com/ghostreborn/authentication/model/Asset.java
+++ src/main/java/com/ghostreborn/authentication/model/Asset.java
@@ -1,7 +1,12 @@
 package com.ghostreborn.authentication.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Asset {
 
+    @Id
     private Long id;
     private String name;
 
