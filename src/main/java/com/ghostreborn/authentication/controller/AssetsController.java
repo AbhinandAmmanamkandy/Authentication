67fb6b9492257d72d8858ce04c1c7365fbfe6978
AbhinandAmmanamkandy <abhinandammanamkandy@gmail.com>
2026-03-11 15:39:23 +0530

    Create template asset to send through RequestMapping

--- src/main/java/com/ghostreborn/authentication/controller/AssetsController.java
+++ src/main/java/com/ghostreborn/authentication/controller/AssetsController.java
@@ -1,11 +1,20 @@
 package com.ghostreborn.authentication.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.ghostreborn.authentication.model.Asset;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/assets")
 public class AssetsController {
 
+    ArrayList<Asset> testAssets = new ArrayList<>(
+            List.of(
+                    new Asset()
+            )
+    );
 
 }
