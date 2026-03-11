4e9de30a9e892497e48ec5627d53436beb0d665b
AbhinandAmmanamkandy <abhinandammanamkandy@gmail.com>
2026-03-11 19:35:28 +0530

    Create findByDeletedFalse query method

--- src/main/java/com/ghostreborn/authentication/repository/AssetRepository.java
+++ src/main/java/com/ghostreborn/authentication/repository/AssetRepository.java
@@ -3,5 +3,8 @@ package com.ghostreborn.authentication.repository;
 import com.ghostreborn.authentication.model.Asset;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AssetRepository extends JpaRepository<Asset, Long> {
+    List<Asset> findByDeletedFalse();
 }
