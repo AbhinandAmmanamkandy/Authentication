b4becff95db677d99154965686222c28e179944f
AbhinandAmmanamkandy <abhinandammanamkandy@gmail.com>
2026-03-11 15:36:44 +0530

    Create route for assets

---
+++ src/main/java/com/ghostreborn/authentication/controller/AssetsController.java
@@ -0,0 +1,11 @@
+package com.ghostreborn.authentication.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/assets")
+public class AssetsController {
+
+
+}
