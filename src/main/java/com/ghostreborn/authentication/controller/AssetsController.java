f640935f69149f4bc406577aba2711768cd7e4d1
AbhinandAmmanamkandy <abhinandammanamkandy@gmail.com>
2026-03-11 19:31:48 +0530

    Create Patch Mapping too

--- src/main/java/com/ghostreborn/authentication/controller/AssetsController.java
+++ src/main/java/com/ghostreborn/authentication/controller/AssetsController.java
@@ -23,10 +23,29 @@ public class AssetsController {
         return assetRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    @ResponseBody
+    public Asset getAssetById(@PathVariable Long id) {
+        return assetRepository.findById(id)
+                .orElse(new Asset());
+    }
+
     @PostMapping
     public String createAsset(Asset asset) {
         assetRepository.save(asset);
         return "redirect:/dashboard";
+    }
+
+    @PatchMapping("/{id}")
+    @ResponseBody
+    public Asset updateAsset(@PathVariable Long id, @RequestBody Asset updatedAsset) {
+        Asset asset = assetRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Asset not found"));
+
+        if (updatedAsset.getName() != null)
+            asset.setName(updatedAsset.getName());
+
+        return assetRepository.save(asset);
     }
 
     @DeleteMapping("/{id}")
