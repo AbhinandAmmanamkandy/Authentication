70bff70890fd619181230575a508565431d10139
AbhinandAmmanamkandy <abhinandammanamkandy@gmail.com>
2026-03-11 15:46:25 +0530

    Mark @Data for getters and setters

--- src/main/java/com/ghostreborn/authentication/model/Asset.java
+++ src/main/java/com/ghostreborn/authentication/model/Asset.java
@@ -3,11 +3,13 @@ package com.ghostreborn.authentication.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Data
 public class Asset {
 
     @Id
