747ea120ee23183812460bd9d1c86e8a6504a836
AbhinandAmmanamkandy <abhinandammanamkandy@gmail.com>
2026-03-11 16:12:52 +0530

    Create UserRepository * Update User model for persistence

--- src/main/java/com/ghostreborn/authentication/model/User.java
+++ src/main/java/com/ghostreborn/authentication/model/User.java
@@ -1,7 +1,14 @@
 package com.ghostreborn.authentication.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
     private Long id;
 
     private String username;
---
+++ src/main/java/com/ghostreborn/authentication/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.ghostreborn.authentication.repository;
+
+import com.ghostreborn.authentication.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
