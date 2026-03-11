d7338ba78dd63726078cad9763f0cf0ceb131f8b
AbhinandAmmanamkandy <abhinandammanamkandy@gmail.com>
2026-03-11 18:21:30 +0530

    Set id as generated value

--- src/main/java/com/ghostreborn/authentication/model/Asset.java
+++ src/main/java/com/ghostreborn/authentication/model/Asset.java
@@ -1,6 +1,7 @@
 package com.ghostreborn.authentication.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,6 +14,7 @@ import lombok.NoArgsConstructor;
 public class Asset {
 
     @Id
+    @GeneratedValue
     private Long id;
     private String name;
 }
