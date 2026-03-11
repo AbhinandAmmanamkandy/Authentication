7f76c5b5c75318bc17959c3938a7c25cf1187515
AbhinandAmmanamkandy <abhinandammanamkandy@gmail.com>
2026-03-11 21:11:37 +0530

    Create view asset route

--- src/main/java/com/ghostreborn/authentication/controller/DashboardController.java
+++ src/main/java/com/ghostreborn/authentication/controller/DashboardController.java
@@ -1,12 +1,22 @@
 package com.ghostreborn.authentication.controller;
 
+import com.ghostreborn.authentication.model.Asset;
+import com.ghostreborn.authentication.repository.AssetRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/dashboard")
 public class DashboardController {
+
+    private final AssetRepository assetRepository;
+
+    public DashboardController(AssetRepository assetRepository) {
+        this.assetRepository = assetRepository;
+    }
 
     @GetMapping
     public String dashboard() {
@@ -16,6 +26,13 @@ public class DashboardController {
     @GetMapping("/create")
     public String createAsset() {
         return "create_asset";
+    }
+
+    @GetMapping("/view/{id}")
+    public Asset viewAsset(
+            @PathVariable Long id
+    ) {
+        return assetRepository.findById(id).orElse(new Asset());
     }
 
 }
