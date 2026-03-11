5d1a54e27dcc4c9c5b49c69329c2b3f1a47fe3d2
AbhinandAmmanamkandy <abhinandammanamkandy@gmail.com>
2026-03-11 16:43:56 +0530

    Controller: After register redirect to login

--- src/main/java/com/ghostreborn/authentication/controller/AuthController.java
+++ src/main/java/com/ghostreborn/authentication/controller/AuthController.java
@@ -1,16 +1,33 @@
 package com.ghostreborn.authentication.controller;
 
+import com.ghostreborn.authentication.model.User;
+import com.ghostreborn.authentication.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/auth")
 public class AuthController {
 
+    private final UserRepository userRepository;
+
+    public AuthController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(User user) {
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+        userRepository.save(user);
+        return "redirect:/auth/login";
     }
 
 }
