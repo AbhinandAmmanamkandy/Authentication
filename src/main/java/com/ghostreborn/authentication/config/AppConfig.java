4f571a2247cc3622a3f5099381446d4ffa629036
AbhinandAmmanamkandy <abhinandammanamkandy@gmail.com>
2026-03-11 16:54:53 +0530

    Update filter chain to allow and use custom authentication htmls

--- src/main/java/com/ghostreborn/authentication/config/AppConfig.java
+++ src/main/java/com/ghostreborn/authentication/config/AppConfig.java
@@ -1,7 +1,20 @@
 package com.ghostreborn.authentication.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class AppConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth.
+                        requestMatchers("/auth/**").permitAll()
+                        .anyRequest()
+                        .authenticated());
+        return http.build();
+    }
+
 }
