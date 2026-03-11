4e7b5f5b43e14e969ee6da3868647c484b4ee66f
AbhinandAmmanamkandy <abhinandammanamkandy@gmail.com>
2026-03-11 16:28:35 +0530

    CustomUserService to custom authenticate by username

---
+++ src/main/java/com/ghostreborn/authentication/service/CustomUserService.java
@@ -0,0 +1,22 @@
+package com.ghostreborn.authentication.service;
+
+import com.ghostreborn.authentication.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
